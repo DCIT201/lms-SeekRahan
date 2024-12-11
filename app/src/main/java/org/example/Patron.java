782fc49155059979b0e78ea5eab0dbc81f0f2f0b
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-11 10:44:22 +0000

    Update Patron.java

--- app/src/main/java/org/example/Patron.java
+++ app/src/main/java/org/example/Patron.java
@@ -1,7 +1,24 @@
-public Patron{
+public Patron extends Library{
   private String patronId;
   private String name;
   private String contact;
   private String booksBorrowed;
+
+    // Constructor
+    public Patron(String name, String patronId, String contact) {
+        this.name = name;
+        this.id = id;
+        this.contact = contact;
+    }
+
   
+    public void borrowBook(Library library, String bookId ) {
+        
+    }
+
+    public void returnBook(Library library, String bookId) {
+        
+    }
 }
+  
+
