9d61ecada393f259271663b05fbf61d3aed830a5
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-11 10:56:05 +0000

    Update Library.java

--- app/src/main/java/org/example/Library.java
+++ app/src/main/java/org/example/Library.java
@@ -1,22 +1,19 @@
 public class Library {
     private ArrayList<Book> books;
 
-    // Constructor
+    
     public Library() {
         books = new ArrayList<>();
     }
 
-    // Methods to add, remove, and search for books
+   
     public void addBook(Book book) {
         books.add(book);
     }
 
     public void removeBook(String bookId) {
-        // Implement logic to remove a book by ISBN
+        books.remove(bookId);
+        
     }
 
-    public Book searchBook(String title) {
-        // Implement logic to search for a book by title
-        return null;
-    }
 }
