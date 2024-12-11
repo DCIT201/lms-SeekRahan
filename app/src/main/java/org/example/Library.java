80fe3444ec94f319c6b0f29affce2e53adc526bd
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-11 12:04:17 +0000

    Update Library.java

--- app/src/main/java/org/example/Library.java
+++ app/src/main/java/org/example/Library.java
@@ -1,19 +1,21 @@
+import java.util.ArrayList;
+
 public class Library {
     private ArrayList<Book> books;
 
-    
+
     public Library() {
         books = new ArrayList<>();
     }
 
-   
+
     public void addBook(Book book) {
         books.add(book);
     }
 
-    public void removeBook(String bookId) {
-        books.remove(bookId);
-        
+    public void removeBook(Book book) {
+        books.remove(book);
+
     }
 
 }
