c53ff5f92762a14aa6db46dc73be052e199c0e3e
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-11 10:49:09 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,6 +1,4 @@
-/*
- * This should be your main class where all your objects will be created
- */
+
 package org.example;
 
 public class App {
@@ -10,5 +8,20 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        
+        Library library = new Library();
+        Patron patron1 = new Patron("Alice", 123);
+        Patron patron2 = new Patron("Bob", 456);
+
+        library.addBook(new Book("ISBN123", "The Lord of the Rings", "J.R.R. Tolkien"));
+        library.addBook(new Book("ISBN456", "Harry Potter", "J.K. Rowling"));
+
+        patron1.borrowBook(library, "ISBN123");
+
+        patron2.borrowBook(library, "ISBN123"); 
+
+        patron1.returnBook(library, "ISBN123");
     }
 }
+    
