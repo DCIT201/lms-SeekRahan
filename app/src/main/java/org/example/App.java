806c80b22afaff7d4134dd9dc8f9081762cded54
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-11 12:01:35 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,19 +9,19 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
-        
+
         Library library = new Library();
-        Patron patron1 = new Patron("Alice", 123);
-        Patron patron2 = new Patron("Bob", 456);
+        Patron patron1 = new Patron("Alice", "123","alic@hi.com","02038489");
+        Patron patron2 = new Patron("Bob", "456","bob@hrt.com","0283449");
 
         library.addBook(new Book("ISBN123", "The Lord of the Rings", "J.R.R. Tolkien"));
         library.addBook(new Book("ISBN456", "Harry Potter", "J.K. Rowling"));
 
-        patron1.borrowBook(library, "ISBN123");
+        patron1.borrowBook("ISBN123");
 
-        patron2.borrowBook(library, "ISBN123"); 
+        patron2.borrowBook("ISBN123");
 
-        patron1.returnBook(library, "ISBN123");
+        patron1.returnBook( "ISBN123");
     }
 }
     
