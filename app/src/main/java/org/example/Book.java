0f83dd8d91cbb88ccee17a0d17ceb9e8aa045430
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-18 09:11:01 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,42 +1,13 @@
-
 public class Book {
-    private final String bookId;
-    private final String title;
-    private String genre;
-    private final String author;
-    private boolean status;
+package org.example;
 
+    // This is your working class.
 
+/**
+ * @param yearPublished Add this field
+ */
+public record Book(String title, String author, int yearPublished) {
     // Constructor
-    public Book(String bookId, String title, String author) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-        this.status = true;
-    }
-
-    // Getters and Setters (Encapsulation)
-    public String getBookId() {
-        return bookId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
+    // Initialize the field
 }
-
-
-
     
