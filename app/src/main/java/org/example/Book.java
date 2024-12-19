06db5842e509ba84d637248c35a9782e308141de
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-19 10:29:08 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -4,7 +4,20 @@ public class Book {
     private String author;
     private int yearPublished;
     // This is your working class
-
+    
+    //setters
+     public set setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setYearPublished(){
+        this.yearPublished = yearPublished;
+    }
+    
+    //getters
     public String getTitle(){
         return title;
     }
