c2c301c0a672ff5029d456dc6796f7aa26b3ebdf
SeekRahan <rushdan.ibnantiku@gmail.com>
2024-12-19 09:36:33 +0000

    Update Library.java

--- app/src/main/java/org/example/Library.java
+++ app/src/main/java/org/example/Library.java
@@ -4,26 +4,29 @@ import java.util.ArrayList;
 
 public class Library {
     private final ArrayList<Book> books;
+    
 
     public Library() {
         books = new ArrayList<>();
     }
 
+    
     public void addBook(Book book) {
         books.add(book);
     }
+    
 
     public boolean removeBook(String title) {
         return books.removeIf(book -> book.title().equals(title));
     }
 
+    
     public void listBooks() {
+        
         if (books.isEmpty()) {
             System.out.println("No books available.");
-        } else {
-            for (Book book : books) {
+        } else {for (Book book : books) {
                 System.out.println(book.title());
-            }
-        }
+        }     
     }
 }
